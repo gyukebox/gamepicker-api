cf401640c90c567d430e0f351622aaad895f23ce
smk0107 <smk0107@smk0107.local>
2018-09-29 13:55:20 +0900

    fix platforms

--- routes/platforms.js
+++ routes/platforms.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const mysql = require('mysql');
-const dbConfig = require('../config/db-config');
 
 const DATABASE = require('../model/Database');
 const database = DATABASE();
