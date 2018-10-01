94413ad634805b5b492c5305f516a9a1ea84f096
smk0107 <smk0107@smk0107.local>
2018-10-01 11:13:37 +0900

    fix model

--- model/Database.js
+++ model/Database.js
@@ -24,7 +24,7 @@ class Database {
             })
         })
     }
-    exist(field, data) {
+    unique(field, data) {
         return new Promise((resolve, reject) => {
             this.connection.query(`SELECT count(*) as count FROM accounts WHERE ${field}='${data}'`, (err, rows) => {
                 if (err)    return reject(err);
