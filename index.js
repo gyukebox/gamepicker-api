ee1509ae67031a9e3c8c7884048852d239fe7f10
smk0107 <smk0107@smk0107.local>
2018-10-08 22:43:33 +0900

    fix lost connection

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const cors = require('cors');
 const passport = require('passport');
 const auth = require('./routes/passport')();
 
+const mysql = require('mysql');
+const db_config = require('./config/db-config');
+
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -18,6 +21,33 @@ app.use('/games', require('./routes/game'));
 app.use('/tags', require('./routes/tags'));
 app.use('/platforms', require('./routes/platforms'));
 app.use('/talks', require('./routes/talk'));
+
+
+
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
 
 //test code
 app.get('/', (req, res) => {
