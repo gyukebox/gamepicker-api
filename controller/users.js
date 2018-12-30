d2047088a15a45d45d4c8c6b9da56ce78b12565c
ansrl0107 <ansrl0107@gmail.com>
2018-12-30 17:08:26 +0900

    fix

--- controller/users.js
+++ controller/users.js
@@ -264,7 +264,8 @@ router.get('/:user_id/games/rating', (req, res) => {
         const sql = `
         SELECT 
             game_reviews.game_id, AVG(score) AS score, 
-            (SELECT JSON_ARRAYAGG(value) FROM game_tags LEFT JOIN tags ON tags.id = game_tags.tag_id WHERE game_tags.game_id = game_reviews.game_id) AS tags        
+            (SELECT JSON_ARRAYAGG(value) FROM game_tags LEFT JOIN tags ON tags.id = game_tags.tag_id WHERE game_tags.game_id = game_reviews.game_id) AS tags,
+            (SELECT JSON_ARRAYAGG(value) FROM game_platforms LEFT JOIN platforms ON platforms.id = game_platforms.platform_id WHERE game_platforms.game_id = game_reviews.game_id) AS platofrms 
         FROM game_reviews 
         WHERE game_reviews.user_id = ?
         GROUP BY game_reviews.game_id
