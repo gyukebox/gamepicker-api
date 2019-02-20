a89caf2e8e4997fd6e8ed19cfbb5a010648bbaf5
smk0107 <ansrl0107@gmail.com>
2019-02-20 22:21:21 +0900

    푸시알림테스트

--- controller/notification.js
+++ controller/notification.js
@@ -2,16 +2,11 @@ const FCM = require('fcm-push');
 const fcm_config = require('../config/push');
 const fcm = new FCM(fcm_config.secret);
 
-module.exports = async (reg_id, title, msg) => {
+module.exports = async (reg_id, notification, data) => {
     const push_data = {
         to: reg_id,
-        notification: {
-            title: title,
-            body: msg,
-            sound: 'default',
-            click_action: "FCM_PLUGIN_ACTIVITY",
-            icon: "fcm_push_icon"
-        }
+        notification,
+        data
     }
-    const push_result = await fcm.send(push_data);    
+    await fcm.send(push_data);
 }
