37413feca1969905f5fa18b9b1b643b14434e12e
smk0107 <ansrl0107@gmail.com>
2019-03-05 23:23:26 +0900

    최대용량 증감

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ const me = require('./routes/me');
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 app.use(bodyParser.json());
 app.get('/favicon.ico', (req, res) => {
     res.sendFile(__dirname + '/public/images/favicon.ico')
