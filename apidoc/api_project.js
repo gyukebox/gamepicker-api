fbe3e05292dd468d4dade5ae75ba7931fe7963ba
smk0107 <ansrl0107@gmail.com>
2019-03-12 20:46:07 +0900

    apidoc 제목 변경

--- apidoc/api_project.js
+++ apidoc/api_project.js
@@ -1,7 +1,7 @@
 define({
+  "name": "GamePicker API",
   "title": "Gamepicker API Documents",
   "url": "https://api.gamepicker.co.kr",
-  "name": "gamepickerapi_v2",
   "version": "1.0.0",
   "description": "",
   "sampleUrl": false,
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-03-12T11:39:10.993Z",
+    "time": "2019-03-12T11:45:20.532Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
