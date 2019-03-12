56ac8a6e039de89d3f8a3972f843f96bcd8e8806
smk0107 <smk0107@smk0107-MBP.local>
2019-03-13 01:33:35 +0900

    gitignore 변경

--- apidoc/api_project.js
+++
@@ -1,1 +0,0 @@
-define({  "name": "Gamepicker API",  "title": "Gamepicker API Documents",  "url": "https://api.gamepicker.co.kr",  "version": "1.0.0",  "description": "",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2019-03-12T16:26:22.609Z",    "url": "http://apidocjs.com",    "version": "0.17.7"  }});
